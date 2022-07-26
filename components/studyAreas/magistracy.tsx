1cfd645241c6d3f67ac78e7e32f4d237c7d2beae
Nikita <kobzevnikita0@mail.ru>
2022-07-26 15:48:00 +0300

    fixed import bug

--- components/studyAreas/magistracy.tsx
+++ components/studyAreas/magistracy.tsx
@@ -7,7 +7,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { StringMap } from "react-admin";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
