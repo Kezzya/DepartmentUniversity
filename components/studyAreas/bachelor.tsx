1e7328bbe97cbfdb89314272e5f9712ccbbc7794
Nikita <kobzevnikita0@mail.ru>
2022-07-26 15:46:43 +0300

    fixed import bug

--- components/studyAreas/bachelor.tsx
+++ components/studyAreas/bachelor.tsx
@@ -7,8 +7,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { StringMap } from "react-admin";
-import { yellow } from "@mui/material/colors";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
