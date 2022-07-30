f1f3ad5e5825014f02dc35d224ae70273af3be88
Nikita <kobzevnikita0@mail.ru>
2022-07-30 12:31:03 +0300

    deleted styles in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,11 +20,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <style jsx global>{`
-            #__next {
-              height: 100%;
-            }
-          `}</style>
         </body>
       </Html>
     );
